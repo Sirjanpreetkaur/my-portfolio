1972d666aa5410c4629a57a61e2a391da7b4d4bc
Sirjanpreet Kaur <sirjanpreetkaur13@gmail.com>
2025-02-28 12:12:26 -0800

    header component created

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import Header from '../components/header';
+
+const PortfolioWebsite=()=>{
+    return(
+        <div className='website'>
+            <Header/>
+        </div>
+    )
+
+}
+export default PortfolioWebsite;
