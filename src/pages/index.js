cfc93b89c99e0db1c43aeaafd241348d49c82b63
Sirjanpreet Kaur <sirjanpreetkaur13@gmail.com>
2025-03-02 13:40:15 -0800

    skills section added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../components/header';
+import Skills from '../components/skills';
 
 const PortfolioWebsite=()=>{
     return(
         <div className='website'>
             <Header/>
+            <Skills/>
         </div>
     )
 
