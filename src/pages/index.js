db9e9cca09b8a3cdb50c33103b90303751e8167f
Sirjanpreet Kaur <sirjanpreetkaur13@gmail.com>
2025-03-07 10:08:20 -0800

    resume component added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../components/header';
 import Skills from '../components/skills';
+import Resume from '../components/resume';
 
 const PortfolioWebsite=()=>{
     return(
         <div className='website'>
             <Header/>
             <Skills/>
+            <Resume/>
         </div>
     )
 
