83ddec3120891348f839a0b84efa0738aab2b61d
Sirjanpreet Kaur <sirjanpreetkaur13@gmail.com>
2025-03-13 14:50:08 -0700

    experience section wip

---
+++ src/components/experience.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import "../assets/css/portfolio.css"
+
+
+
+const Experience = () => {
+    return (
+        <div className='experience-cover'>
+            <div className='experience-years'>
+                3 Years of Experience
+            </div>
+            <div class="experience-list">
+                <div class="experience-roles">
+                    <div class="experience-role">
+                        <strong>SDE || (Frontend)</strong>
+                        <div>Dec/2022 - present</div>
+                    </div>
+                    <div>
+                        <h5>CAREERS360</h5>
+                    </div>
+                </div>
+                <div class="experience-roles">
+                    <div class="experience-role">
+                        <strong>Frontend Developer</strong>
+                        <div>June/2022 - August/2024</div>
+                    </div>
+                    <div>
+                        <h5>EClerx</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Experience
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import Header from '../components/header';
-import Skills from '../components/skills';
-import Resume from '../components/resume';
+import React, { useEffect, useState } from "react";
+import Header from "../components/header";
+import Skills from "../components/skills";
+import Resume from "../components/resume";
+import Experience from "../components/experience";
 
-const PortfolioWebsite=()=>{
-    return(
-        <div className='website'>
-            <Header/>
-            <Skills/>
-            <Resume/>
-        </div>
-    )
-
-}
+const PortfolioWebsite = () => {
+  return (
+    <div className="website">
+      <Header />
+      <Skills />
+      <div className="exp-cvr">
+        <Experience />
+        <Resume />
+      </div>
+    </div>
+  );
+};
 export default PortfolioWebsite;
