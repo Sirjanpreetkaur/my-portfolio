896c81c8cf14555462c0bccad2b7ca5cdd3aa406
Sirjanpreet Kaur <sirjanpreetkaur13@gmail.com>
2025-03-25 11:33:35 -0700

    navbar component added

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "../assets/css/portfolio.css";
+import { MdOutlineLightMode } from "react-icons/md";
+import { MdOutlineDarkMode } from "react-icons/md";
 
-const NavBar = () => {
+const NavBar = ({ activeSection,setActiveSection, theme, setTheme }) => {
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+  
   return (
-    <>
-      <div>Navbar</div>
-    </>
-  )
-}
+    <div className="nav_bar" id="navbarNav">
+      <div>
+        <ul className="nav_list">
+          <li className={`item ${activeSection === 'profile' ? 'active' : ''}`} onClick={() => setActiveSection('profile')}>
+            <p>
+              Profile
+            </p>
+          </li>
+          <li className={`item ${activeSection === 'tools' ? 'active' : ''}`} onClick={() => setActiveSection('tools')}>
+            <p>
+              Tools
+            </p>
+          </li>
+          <li className={`item ${activeSection === 'experience' ? 'active' : ''}`} onClick={() => setActiveSection('experience')}>
+            <p>
+              Experience
+            </p>
+          </li>
+          <li className={`item ${activeSection === 'work' ? 'active' : ''}`} onClick={() => setActiveSection('work')}>
+            <p>
+              Work
+            </p>
+          </li>
+        </ul>
+      </div>
+      <div>
 
-export default NavBar
+        <ul className="nav_list theme" onClick={toggleTheme}>
+          <li className="item">
+            <p>
+              {theme === "light" ? <MdOutlineDarkMode /> : <MdOutlineLightMode />}
+            </p>
+          </li>
+          <span className="glider"></span>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
