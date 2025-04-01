7a12f7f3dbcb3dbfd0fd88fe6c5846aebbc1a836
Sirjanpreet Kaur <sirjanpreetkaur13@gmail.com>
2025-04-01 15:10:02 -0700

    welcome strip added

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,19 +6,44 @@ import Experience from "../components/experience";
 import Projects from "../components/projects";
 import StaticPart from "../components/staticPart";
 import ContactMe from "../components/contactMe";
+import WelcomeStrip from "../components/welcomeStrip";
 
 const PortfolioWebsite = () => {
+  const [company, setCompany] = useState("");
+  const [showStrip, setShowStrip] = useState(true);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    let companyValue = params.get("company");
+    if (companyValue) {
+      companyValue = companyValue.replace(/-/g, " ");
+      setCompany(companyValue);
+
+      const timer = setTimeout(() => {
+        setShowStrip(false);
+        setTimeout(() => {
+          setCompany("");
+          setShowStrip(true);
+        }, 1000);
+      }, 10000);
+
+      return () => clearTimeout(timer);
+    }
+  }, []);
   return (
     <div className="website">
+      {company && showStrip && (
+        <WelcomeStrip company={company} fadeOut={!showStrip} />
+      )}
       <Header />
       <Skills />
       <div className="exp-cvr">
         <Experience />
         <Resume />
       </div>
-      <Projects/>
-      <StaticPart/>
-      <ContactMe/>
+      <Projects />
+      <StaticPart />
+      <ContactMe />
     </div>
   );
 };
