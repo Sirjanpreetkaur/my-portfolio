cd0b5d73e7aa58ca541df69453be6933b4685839
Sirjanpreet Kaur <sirjanpreetkaur13@gmail.com>
2025-04-11 15:47:23 -0700

    image height fixed for mobile

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,45 @@
-import React from 'react'
-import "../assets/css/portfolio.css"
-import profileImage from "../assets/images/profileImage.png"
+import React from "react";
+import "../assets/css/portfolio.css";
+import profileImage from "../assets/images/profileImage.png";
 
 const Header = () => {
   return (
     <>
-      <div className='portfolio-image-cover'>
-        <div className='portfolio-intro'>
+      <div className="portfolio-image-cover">
+        <div className="portfolio-intro">
           <div class="content">
             <div class="content-tertiary">
-              <p><span>✌🏻</span> Hi there! I'm Sirjanpreet Kaur</p>
+              <p>
+                <span>✌🏻</span> Hi there! I'm Sirjanpreet Kaur
+              </p>
             </div>
             <div class="content-primary">
-              <h1>A passionate <span>Frontend Developer</span>
-               </h1>
+              <h1>
+                <span>Frontend Developer</span>
+              </h1>
             </div>
             <div class="content-secondary">
-              <p>I'm a Frontend Developer with real-world experience in building fast, responsive, and accessible UIs. At Careers360, I contributed to high-impact projects using React.js and SSR techniques, ensuring smooth experiences across platforms.
+              <p>
+                <p>
+                I design and build fast, scalable, and user-friendly web apps
+                that don’t just look good, they work flawlessly. I specialize in
+                frontend development, crafting clean, modular React and Next.js
+                interfaces with smooth UX and performance in mind.</p>
+               
+                <p>
+                  From pixel-perfect UI to debugging tough issues, I turn
+                  complex requirements into reliable, maintainable code.
+                </p>
               </p>
             </div>
           </div>
-          <div className='portfolio-profile-img'>
+          <div className="portfolio-profile-img">
             <img src={profileImage} alt="portfolio-image" />
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
