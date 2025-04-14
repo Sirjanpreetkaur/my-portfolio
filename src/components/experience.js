60845bcbc69481760886820cdfd8a6de034f30b7
Sirjanpreet Kaur <sirjanpreetkaur13@gmail.com>
2025-04-14 15:40:26 -0700

    favicon and experience changed

--- src/components/experience.js
+++ src/components/experience.js
@@ -8,13 +8,13 @@ const Experience = () => {
     return (
         <div className='experience-cover'>
             <div className='experience-years'>
-                3 Years of Experience
+                3.6 Years of Experience
             </div>
             <div class="experience-list">
                 <div class="experience-roles">
                     <div class="experience-role">
                         <strong>SDE 2 (Frontend)</strong>
-                        <div>Dec/2022 - present</div>
+                        <div>Dec/2021 - present</div>
                     </div>
                     <div>
                         <h5>CAREERS360</h5>
@@ -23,7 +23,7 @@ const Experience = () => {
                 <div class="experience-roles">
                     <div class="experience-role">
                         <strong>Frontend Developer</strong>
-                        <div>June/2022 - August/2024</div>
+                        <div>June/2021 - August/2021</div>
                     </div>
                     <div>
                         <h5>EClerx</h5>
