9d0bea6e9848a2f5f3c189e9eb3aa9c7c2ee930f
Sirjanpreet Kaur <sirjanpreetkaur13@gmail.com>
2025-04-21 00:02:11 -0700

    minor fixes

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,37 +1,43 @@
-import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import { FaEye, FaDownload } from "react-icons/fa";
-import "../assets/css/portfolio.css"
-import ResumeCV from "../assets/pdf/resume.pdf"
+import "../assets/css/portfolio.css";
+import ResumeCV from "../assets/pdf/resume.pdf";
 
-const resumeUrl = "https://drive.google.com/file/d/1YzsmnF5f1UHLCzywZAs9xoFHxKsL6mFj/view?usp=sharing";
+const resumeUrl =
+  "https://drive.google.com/file/d/1YzsmnF5f1UHLCzywZAs9xoFHxKsL6mFj/view?usp=sharing";
 
 const Resume = () => {
-    const downloadPDF = async (pdfUrl) => {
-        try {
-            const response = await fetch(pdfUrl);
-            const blob = await response.blob();
-            const url = window.URL.createObjectURL(new Blob([blob]));
-            const link = document.createElement('a');
-            link.href = url;
-            const timestamp = new Date().getTime();
-            link.setAttribute('download', `${"resume"}_${timestamp}.pdf`);
-            document.body.appendChild(link);
-            link.click();
-            link.parentNode.removeChild(link);
-        } catch (error) {
-            console.error('Error downloading the PDF:', error);
-        }
+  const downloadPDF = async (pdfUrl) => {
+    try {
+      const response = await fetch(pdfUrl);
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(new Blob([blob]));
+      const link = document.createElement("a");
+      link.href = url;
+      const timestamp = new Date().getTime();
+      link.setAttribute("download", `${"resume"}_${timestamp}.pdf`);
+      document.body.appendChild(link);
+      link.click();
+      link.parentNode.removeChild(link);
+    } catch (error) {
+      console.error("Error downloading the PDF:", error);
+    }
   };
 
   return (
-        <div className='experience-cover'>
+    <div className="experience-cover">
       <div className="download_resume">
         <span>My Resume</span>
-        <a href={resumeUrl} target="_blank" rel="noopener noreferrer" className="view-resume">
+        <a
+          href={resumeUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="view-resume"
+        >
           <FaEye />
         </a>
-                <a onClick={() => downloadPDF(ResumeCV)} className="download-resume">
+        <a onClick={() => downloadPDF(ResumeCV)} className="download-resume">
           <FaDownload />
         </a>
       </div>
