d590b4e83e1398308d8288510cd1312bb9ac5757
Sirjanpreet Kaur <92227890+Sirjanpreetkaur@users.noreply.github.com>
2025-07-26 15:40:07 +0530

    Update experience.js

--- src/components/experience.js
+++ src/components/experience.js
@@ -8,13 +8,13 @@ const Experience = () => {
     return (
         <div className='experience-cover'>
             <div className='experience-years'>
-                3.7 Years of Experience
+                3 Years of Experience
             </div>
             <div className="experience-list">
                 <div className="experience-roles">
                     <div className="experience-role">
                         <strong>SDE 2 (Frontend)</strong>
-                        <div>Dec/2021 - present</div>
+                        <div>Dec/2022 - present</div>
                     </div>
                     <div>
                         <h5>CAREERS360</h5>
@@ -23,7 +23,7 @@ const Experience = () => {
                 <div className="experience-roles">
                     <div className="experience-role">
                         <strong>Frontend Developer</strong>
-                        <div>June/2021 - August/2021</div>
+                        <div>June/2022 - August/2022</div>
                     </div>
                     <div>
                         <h5>EClerx</h5>
